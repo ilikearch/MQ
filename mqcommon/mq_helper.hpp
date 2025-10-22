3396e701a69184424f8cbc4837c295100159eda6
ilikearch <2743766316@qq.com>
2025-10-22 23:35:53 +0800

    add

---
+++ mqcommon/mq_helper.hpp
@@ -0,0 +1,17 @@
+#ifndef __M_HELPER_H__
+#define __M_HELPER_H__
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <random>
+#include <sstream>
+#include <iomanip>
+#include <atomic>
+#include <sqlite3.h>
+#include <cstring>
+#include <cerrno>
+#include <sys/stat.h>
+#include "mq_logger.hpp"
+
+#endif
